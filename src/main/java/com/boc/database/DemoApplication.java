458a598780a397dfefc28904a3ba229cd70319aa
linmurrays <linmurrays@outlook.com>
2021-02-11 19:41:23 +0800

    2-11

--- src/main/java/com/boc/database/DemoApplication.java
+++ src/main/java/com/boc/database/DemoApplication.java
@@ -1,4 +1,4 @@
-package demo;
+package com.boc.database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/boc/database/DemoApplicationTests.java
+++ src/test/java/com/boc/database/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package demo;
+package com.boc.database;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
